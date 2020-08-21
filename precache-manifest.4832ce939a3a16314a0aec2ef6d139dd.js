e3db1879672d71eee90abc6ee2419ca01a04f430
Abednego Chase <achase@Abednegos-MacBook-Pro.local>
2020-08-21 03:16:22 -0400

    Updates

---
+++ precache-manifest.4832ce939a3a16314a0aec2ef6d139dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542bce76ff690821c40501bd33832019",
+    "url": "/BreakingBadCast/index.html"
+  },
+  {
+    "revision": "c29a99fefd640598722b",
+    "url": "/BreakingBadCast/static/css/main.c616ca8d.chunk.css"
+  },
+  {
+    "revision": "5ba6b8b643d7d0e7e15f",
+    "url": "/BreakingBadCast/static/js/2.ff55e753.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BreakingBadCast/static/js/2.ff55e753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29a99fefd640598722b",
+    "url": "/BreakingBadCast/static/js/main.34c5933e.chunk.js"
+  },
+  {
+    "revision": "68d3d15afa842c2fde68",
+    "url": "/BreakingBadCast/static/js/runtime-main.374a6df0.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/BreakingBadCast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/BreakingBadCast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/BreakingBadCast/static/media/spinner.235b9b3e.gif"
+  }
+]);
